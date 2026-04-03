190cf2063aef131183dd3152dce7bc760319ec3a
Naveensharma26 <nssharma309@gmail.com>
2026-04-03 20:32:28 +0530

    Adding image

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -6,7 +6,8 @@ function LeftPanel({ setOpen, setManageOpen }) {
   return (
     <div className="md:min-h-screen bg-blue-950 flex flex-col items-center">
       <div className="sticky top-0 bg-blue-950 w-full flex md:flex-col flex-row items-center gap-2 ">
-        <h1 className="text-yellow-400 text-4xl mt-0 md:mt-10">QuickPoll</h1>
+        <img src="quickpoll.png" alt="" width={80} className="mt-0 md:mt-10" />
+        <h1 className="text-yellow-400 text-4xl">QuickPoll</h1>
         <p className="text-white">Polling Made Quick</p>
       </div>
       <div className="m-8">
